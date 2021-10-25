3b70071f4ed3cb2cf840a81f1107c62a435afc15
Ale Vasquez <jose5m45@gmail.com>
2021-10-24 22:32:07 -0600

    Created backend.

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a6e2f318cb88f18f43c2a78cf4f87482'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '91ce937b-1b97-4c8d-9d28-a3045417e439'
+};
