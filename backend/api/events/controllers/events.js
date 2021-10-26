f179ce13b2932a1d12385af6fad054f618c68246
Ale Vasquez <jose5m45@gmail.com>
2021-10-25 19:07:46 -0600

    added backend router me

--- backend/api/events/controllers/events.js
+++ backend/api/events/controllers/events.js
@@ -1,8 +1,33 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils')
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  // Get logged in users
+  async me(ctx) {
+    const user = ctx.state.user.user
+    if(!user){
+      return ctx.badRequest(
+        null,
+        {messages: [ {id: 'No Authorization Header'}]}
+      )
+    }
+
+    const data = await strapi.services.events.find({user: user.id})
+    if(!data){
+      return ctx.notFound('User not found')
+    }
+
+    return sanitizeEntity(
+      data,
+      {
+        model: strapi.models.events
+      }
+    )
+
+  }
+};
