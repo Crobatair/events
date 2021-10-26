e46923f2c43c109366cec48cc8d0b0fa4277c6eb
Ale Vasquez <jose5m45@gmail.com>
2021-10-25 18:50:40 -0600

    logout & token destruction.

--- events-frontend/context/AuthContext.js
+++ events-frontend/context/AuthContext.js
@@ -38,6 +38,15 @@ export const AuthProvider = ({children}) => {
   }
 
   const logout = async () => {
+    const res = await fetch(`${NEXT_URL}/api/logout`,{
+      method: 'POST'
+    })
+
+    if(res.ok){
+      setUser(null)
+      router.push('/')
+    }
+
 
   }
 
